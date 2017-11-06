d9b126a74c532c8e6c8922dfd59d3556db065fda
smithwebtek <brad@smithwebtek.com>
2017-11-05 20:39:28 -0500

    working with events

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App'; 
-import { ProductCategoryRow, ProductRow, ProductTable, SearchBar, FilterableProductTable } from './ProductCategoryRow'
+import ReactDOM from 'react-dom'
+// import { Tickle } from './Tickle'
+import Clicker from './Clicker'
 
-const MOUNT2 = document.querySelector('#root2')
+// const MOUNT2 = document.querySelector('#root2');
+const MOUNT3 = document.querySelector('#root3');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-const PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
+// ReactDOM.render(
+//   <Tickle 
+//   />,
+//   MOUNT2
+// );
 
 ReactDOM.render(
-  <FilterableProductTable 
-    products={PRODUCTS}
-  />,MOUNT2
+  <Clicker 
+  />,
+  MOUNT3
 );
